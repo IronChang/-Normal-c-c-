b8407f9935e6de3ff5467b2b5664b06906fb7783
IronChang <408662026@qq.com>
2020-01-22 20:13:58 +0800

    2

---
+++ 喝汽水.cpp
@@ -0,0 +1,38 @@
+/#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int num = 0;
+//	int count;
+//	while (cin >> num)
+//	{
+//		count = 0;
+//		if (num == 0)
+//		{
+//			break;
+//		}
+//		while (1)
+//		{
+//			if (num == 2)
+//			{
+//				num += 1;
+//			}
+//		
+//			if (num >= 3)
+//			{
+//				num -= 3;
+//				count++;
+//				num += 1;
+//			}
+//			else
+//			{
+//				num = 0;
+//				break;
+//			}
+//		}
+//		cout << count << endl;;
+//	}
+//	system("pause");
+//	return 0;
+//}

fa8f11c8cfd6b146bee537fc1665443a095f28e8
IronChang <408662026@qq.com>
2020-03-20 21:37:44 +0800

    C语言动态内存管理

---
+++ 动态内存管理.c
@@ -0,0 +1,45 @@
+//char *GetMemory(void)
+//{
+//	char p[] = "hello world";
+//	return p;
+//}
+//void Test(void)
+//{
+//	char *str = NULL;
+//	str = GetMemory();
+//	printf(str);
+//}
+
+//void GetMemory(char **p, int num)
+//{
+//	*p = (char *)malloc(num);
+//}
+//void Test(void)
+//{
+//	char *str = NULL;
+//	GetMemory(&str, 100);
+//	strcpy(str, "hello");
+//	printf(str);
+//}
+
+void Test(void)
+{
+	char *str = (char *)malloc(100);
+	strcpy(str, "hello");
+	free(str);
+	if (str != NULL)
+	{
+		strcpy(str, "world");
+		printf(str);
+	}
+}
+
+int main()
+{
+	Test();
+
+
+
+	system("pause");
+	return 0;
+}
